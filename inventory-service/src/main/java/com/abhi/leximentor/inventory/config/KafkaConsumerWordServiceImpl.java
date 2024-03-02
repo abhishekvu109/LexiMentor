d30f8e2ddc974edb1778f9c198a9c4b194adda33
Abhishek <abhishekvu109@gmail.com>
2024-03-02 19:46:15 +0100

    Added the kafka service

--- inventory-service/src/main/java/com/abhi/leximentor/inventory/config/KafkaConsumerWordServiceImpl.java
+++ inventory-service/src/main/java/com/abhi/leximentor/inventory/config/KafkaConsumerWordServiceImpl.java
@@ -3,6 +3,8 @@ package com.abhi.leximentor.inventory.config;
 import com.abhi.leximentor.inventory.constants.ApplicationConstants;
 import com.abhi.leximentor.inventory.dto.WordDTO;
 import com.abhi.leximentor.inventory.service.WordService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +14,11 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@AllArgsConstructor
+@NoArgsConstructor
 public class KafkaConsumerWordServiceImpl {
 
-    private final WordService wordService;
+    private WordService wordService;
 
     @KafkaListener(topics = ApplicationConstants.KAFKA_TOPIC, groupId = ApplicationConstants.KAFKA_GROUP)
     public void consumeEvents(WordDTO wordDTO) {
