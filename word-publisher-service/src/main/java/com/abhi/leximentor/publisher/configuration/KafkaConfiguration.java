0bccb7f026785a8068f17272de564b7b2a3123fc
Abhishek <abhishekvu109@gmail.com>
2024-03-02 15:45:19 +0100

    Added the kafka service

--- word-publisher-service/src/main/java/com/abhi/leximentor/publisher/configuration/KafkaConfiguration.java
+++ word-publisher-service/src/main/java/com/abhi/leximentor/publisher/configuration/KafkaConfiguration.java
@@ -26,7 +26,7 @@ public class KafkaConfiguration {
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, WordDTOSerializer.class.getName());
         return props;
     }
