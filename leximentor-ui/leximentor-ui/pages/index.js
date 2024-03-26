f1c88ed6119b3e9d38ea9e3c8c9a994a8688e4b5
Abhishek <abhishekvu109@gmail.com>
2024-03-26 06:56:55 +0100

    Added a minor fix

--- leximentor-ui/leximentor-ui/pages/index.js
+++ leximentor-ui/leximentor-ui/pages/index.js
@@ -1,13 +1,7 @@
 // Example component using Bootstrap classes
 
 import React from "react";
-import dynamic from "next/dynamic";
-import Script from "next/script";
-import Head from "next/head";
 
-const DynamicComponent = dynamic(() => import("../components/MyComponents"), {
-  ssr: false,
-});
 
 const Home = () => {
   return (
