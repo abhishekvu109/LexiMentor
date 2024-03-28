a24797e00c6ddf40d2b1c5b3badb949e0bec574b
Abhishek <abhishekvu109@gmail.com>
2024-03-28 14:48:31 +0100

    Added the fix for running the services on Docker

--- speech-from-text/main.py
+++ speech-from-text/main.py
@@ -1,24 +1,27 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, Response
 from gtts import gTTS
-import os
+from flask_cors import CORS
+import io
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/text-to-speech', methods=['POST'])
 def text_to_speech():
     data = request.get_json()
     text = data.get('text')
-    lang = data.get('lang', 'en')
 
     # Create a gTTS object
-    tts = gTTS(text=text, lang=lang, slow=False)
+    tts = gTTS(text=text, lang='en', slow=False)
 
-    # Save the audio file
-    tts.save("output.mp3")
+    # Save the audio file to a BytesIO object
+    audio_data = io.BytesIO()
+    tts.write_to_fp(audio_data)
+    audio_data.seek(0)
 
-    # Return the audio file
-    return send_file("output.mp3", as_attachment=True)
+    # Return the audio file with correct content type and filename in Content-Disposition header
+    return send_file(audio_data, mimetype='audio/mp3', as_attachment=True, download_name='output.mp3')
 
 
 if __name__ == "__main__":
