0906c962040b9aca17944f48e80c235b9f168905
Abhishek <abhishekvu109@gmail.com>
2024-04-07 15:59:11 +0200

    Added the CORS config

--- leximentor-ui/leximentor-ui/constants.js
+++ leximentor-ui/leximentor-ui/constants.js
@@ -1,2 +1,3 @@
 export const API_BASE_URL = 'http://192.168.1.2:9191/api';
+export const API_SYNAPSTER_BASE_URL = 'http://192.168.1.2:9292/api';
 export const API_TEXT_TO_SPEECH = 'http://192.168.1.2:8300/text-to-speech';
