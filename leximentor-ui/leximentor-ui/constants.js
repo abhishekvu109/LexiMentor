99e2eb2917605e598cbd791453834c68d885d5a6
Abhishek <abhishekvu109@gmail.com>
2024-04-12 10:03:19 +0200

    Some more changes

--- leximentor-ui/leximentor-ui/constants.js
+++ leximentor-ui/leximentor-ui/constants.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://192.168.1.2:9191/api';
-export const API_SYNAPSTER_BASE_URL = 'http://192.168.1.2:9292/api';
-export const API_TEXT_TO_SPEECH = 'http://192.168.1.2:8300/text-to-speech';
+export const API_BASE_URL = 'http://192.168.1.4:9191/api';
+export const API_SYNAPSTER_BASE_URL = 'http://192.168.1.4:9292/api';
+export const API_TEXT_TO_SPEECH = 'http://192.168.1.4:8300/text-to-speech';
