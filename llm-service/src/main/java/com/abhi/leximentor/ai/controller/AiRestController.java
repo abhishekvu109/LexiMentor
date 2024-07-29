b7c134f5aecaa2a527ff87dfbcc17de2d2621dfe
abhishek-datasense <abhishek.datasense@gmail.com>
2024-07-29 21:54:16 +0200

    Modied the controller code

--- llm-service/src/main/java/com/abhi/leximentor/ai/controller/AiRestController.java
+++ llm-service/src/main/java/com/abhi/leximentor/ai/controller/AiRestController.java
@@ -3,13 +3,10 @@ package com.abhi.leximentor.ai.controller;
 import com.abhi.leximentor.ai.constants.ApplicationConstants;
 import com.abhi.leximentor.ai.constants.UrlConstants;
 import com.abhi.leximentor.ai.dto.MeaningEvaluationDTO;
-import com.abhi.leximentor.ai.rest.ResponseEntityBuilder;
-import com.abhi.leximentor.ai.rest.RestApiResponse;
 import com.abhi.leximentor.ai.service.OllamaLlmChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,10 +22,16 @@ public class AiRestController {
 //        return chatService.getPromptResult(prompt.getText());
 //    }
 
+    //    @PostMapping(value = UrlConstants.EvaluateMeaningPrompts.GENERATE_PROMPT_URL, consumes = ApplicationConstants.MediaType.APPLICATION_JSON, produces = ApplicationConstants.MediaType.APPLICATION_JSON)
+//    public @ResponseBody ResponseEntity<RestApiResponse> evaluateMeaning(@RequestBody MeaningEvaluationDTO dto, @PathVariable String modelName) {
+//        log.info("Received a request to evaluate the meaning using model- {}, prompt- {}", modelName, dto);
+//        MeaningEvaluationDTO meaningEvaluationDTO = chatService.evaluate(dto.getPrompt());
+//        return ResponseEntityBuilder.getBuilder(HttpStatus.CREATED).successResponse(ApplicationConstants.REQUEST_SUCCESS_DESCRIPTION, meaningEvaluationDTO);
+//    }
     @PostMapping(value = UrlConstants.EvaluateMeaningPrompts.GENERATE_PROMPT_URL, consumes = ApplicationConstants.MediaType.APPLICATION_JSON, produces = ApplicationConstants.MediaType.APPLICATION_JSON)
-    public @ResponseBody ResponseEntity<RestApiResponse> evaluateMeaning(@RequestBody MeaningEvaluationDTO dto, @PathVariable String modelName) {
+    public @ResponseBody ResponseEntity<MeaningEvaluationDTO> evaluateMeaning(@RequestBody MeaningEvaluationDTO dto, @PathVariable String modelName) {
         log.info("Received a request to evaluate the meaning using model- {}, prompt- {}", modelName, dto);
         MeaningEvaluationDTO meaningEvaluationDTO = chatService.evaluate(dto.getPrompt());
-        return ResponseEntityBuilder.getBuilder(HttpStatus.CREATED).successResponse(ApplicationConstants.REQUEST_SUCCESS_DESCRIPTION, meaningEvaluationDTO);
+        return ResponseEntity.ok(meaningEvaluationDTO);
     }
 }
