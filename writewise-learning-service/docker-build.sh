eb72ab99ad487798bcdadeac22666c44c6f52ede
abhishek-datasense <abhishek.datasense@gmail.com>
2024-10-05 13:42:51 +0530

    Created a new microservice for the Writing.

--- writewise-learning-service/docker-build.sh
+++ writewise-learning-service/docker-build.sh
@@ -1,3 +1,4 @@
+mvn clean install -DskipTests -U -f pom.xml
 docker build . -t localhost:5000/writewise-learning-service:latest
 docker push localhost:5000/writewise-learning-service:latest
 # shellcheck disable=SC2164
