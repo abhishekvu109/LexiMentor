0733ec9da4a5c09f2b9c182bb4acf40c0cb8d99a
abhishek-datasense <abhishek.datasense@gmail.com>
2025-05-22 16:54:59 +0530

    Added the fix for return type

--- llm-service/src/main/java/com/abhi/leximentor/ai/controller/v2/AiRestControllerV2.java
+++ llm-service/src/main/java/com/abhi/leximentor/ai/controller/v2/AiRestControllerV2.java
@@ -1,15 +1,11 @@
 package com.abhi.leximentor.ai.controller.v2;
 
-import com.abhi.leximentor.ai.constants.ApplicationConstants;
 import com.abhi.leximentor.ai.constants.UrlConstants;
-import com.abhi.leximentor.ai.rest.ResponseEntityBuilder;
-import com.abhi.leximentor.ai.rest.RestApiResponse;
 import com.abhi.leximentor.ai.service.LLMPromptService;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,9 +22,9 @@ public class AiRestControllerV2 {
 
 
     @PostMapping(value = UrlConstants.EvaluateMeaningPrompts.V2.GENERATE_STANDARD_PROMPT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody ResponseEntity<RestApiResponse> generatePromptResponse(@RequestBody PromptRequest promptRequest) {
+    public @ResponseBody ResponseEntity<String> generatePromptResponse(@RequestBody PromptRequest promptRequest) {
         String response = promptService.execute(promptRequest.prompt(), promptRequest.format(), promptRequest.model());
-        return ResponseEntityBuilder.getBuilder(HttpStatus.OK).successResponse(ApplicationConstants.REQUEST_SUCCESS_DESCRIPTION, response);
+        return ResponseEntity.ok(response);
     }
 
     private record PromptRequest(
