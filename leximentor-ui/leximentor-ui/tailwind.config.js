d39caefd427934b9f69009f4e1c954480a0028d9
abhishek-datasense <abhishek.datasense@gmail.com>
2025-12-19 12:48:53 +0530

    Removed leximentor-ui from this package and added more challenges.

--- leximentor-ui/leximentor-ui/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}", "./public/**/*.html",],
-    theme: {
-        extend: {},
-    },
-    darkMode: "class",
-    plugins: [],
-};
