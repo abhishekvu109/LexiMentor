daf343a8075c8079212d5cea11608dd04324ed1b
abhishek-datasense <abhishek.datasense@gmail.com>
2026-01-16 13:42:36 +0530

    LEXI-68: Added a new endpoint for fetching all the users. GET API

--- auth-service/src/main/java/com/abhi/saarthi/auth/service/UserService.java
+++ auth-service/src/main/java/com/abhi/saarthi/auth/service/UserService.java
@@ -2,8 +2,14 @@ package com.abhi.saarthi.auth.service;
 
 import com.abhi.saarthi.auth.dto.AppUser;
 import com.abhi.saarthi.auth.dto.AuthResponse;
+import com.abhi.saarthi.auth.dto.UserDTO;
+
+import java.util.List;
 
 public interface UserService {
     AppUser save(AppUser user);
+
     AuthResponse validateToken(String token);
+
+    List<UserDTO> findAllUsers();
 }
