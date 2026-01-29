09900edc618508ea165de5302330672d1d55ce6b
abhihsek choudhury <abhishekvu109@gmail.com>
2026-01-29 22:57:06 +0530

    LEXI-84: Leximentor fixes.

--- inventory-service/src/test/java/com/abhi/leximentor/inventory/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/abhi/leximentor/inventory/InventoryServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.abhi.leximentor.inventory;
 
+import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InventoryServiceApplicationTests {
 
     @Test
