ef8937461d754c5488b492c143102af8966060ff
abhihsek choudhury <abhishekvu109@gmail.com>
2026-02-03 14:54:55 +0530

    LEXI-83: Important cashflow fixes.

--- cashflow-service/src/main/java/com/abhi/saarthi/cashflow/service/impl/DashboardServiceImpl.java
+++ cashflow-service/src/main/java/com/abhi/saarthi/cashflow/service/impl/DashboardServiceImpl.java
@@ -23,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Slf4j
@@ -61,6 +62,7 @@ public class DashboardServiceImpl implements DashboardService {
         List<ExpenseDTO> transactionsLast30Days = expenses.stream()
                 .filter(expense -> expense.getExpenseDate() != null && !expense.getExpenseDate().isBefore(LocalDate.now().minusDays(30)))
                 .map(expenseMapper::toDto)
+                .sorted(Comparator.comparing(ExpenseDTO::getExpenseDate).reversed())
                 .toList();
         long todayTransactions = transactionsLast30Days.stream().filter(expenseDTO -> expenseDTO.getExpenseDate() != null && expenseDTO.getExpenseDate().equals(LocalDate.now())).count();
         List<SpendingTrendDTO> spendingTrendDTOS = transactionsLast30Days.stream().map(expenseDTO -> SpendingTrendDTO.builder().amount(expenseDTO.getAmount()).transactionDate(expenseDTO.getExpenseDate()).build()).toList();
